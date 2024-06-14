bc4f9694174d750b1b93571a9ce067760250ab15
Erik Wiberg <erik.wiberg@funkismultimedia.se>
2024-06-14 22:06:20 +0200

    test to see if react works as expected

--- public/src/editor/Editor.tsx
+++ public/src/editor/Editor.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
+const { useState, useEffect } = React;
 
 export default function Editor() {
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    console.log("counter", counter);
+  }, [counter]);
   return (
     <div>
       <h1>Editor</h1>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <pre>{JSON.stringify({ counter }, null, 2)}</pre>
     </div>
   );
 }
