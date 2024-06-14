03f839ae041ba9397f8ba5e2a735b54709e665a3
Erik Wiberg <erik.wiberg@funkismultimedia.se>
2024-06-14 23:06:24 +0200

    working ts when uploading once.. Seems to only work when scripts etc are inlined (look in html view)

--- public/index.tsx
+++ public/index.tsx
@@ -1,6 +1,6 @@
 import "./style.css";
 import "regenerator-runtime/runtime.js";
-import React from "react";
+import * as React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./src/App.tsx";
 
